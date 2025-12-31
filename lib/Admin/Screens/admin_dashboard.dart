5e6949c37f1974054c4cfab02e8050741d26b16e
Vignesh Parmar <vigneshparmar562@gmail.com>
2025-12-31 19:26:56 +0530

    work with Admin

---
+++ lib/Admin/Screens/admin_dashboard.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AdminDashboard extends StatelessWidget {
+  const AdminDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF0E1328),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text("Admin Dashboard"),
+      ),
+      body: const Center(
+        child: Text(
+          "Admin Dashboard Screen",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
