4b38e1eaafdd42b9fe1aff291442e80cac9bd3f7
Vignesh Parmar <vigneshparmar562@gmail.com>
2026-01-03 20:02:28 +0530

    new widget created

---
+++ lib/Admin/Screens/glass.dart
@@ -0,0 +1,28 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+Widget glass({
+  required Widget child,
+  BorderRadiusGeometry borderRadius = BorderRadius.zero,
+  EdgeInsets padding = const EdgeInsets.all(14),
+  BoxShape shape = BoxShape.rectangle,
+}) {
+  return ClipRRect(
+    borderRadius: shape == BoxShape.circle ? null : borderRadius,
+    child: BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 18, sigmaY: 18),
+      child: Container(
+        padding: padding,
+        decoration: BoxDecoration(
+          shape: shape,
+          borderRadius: shape == BoxShape.circle ? null : borderRadius,
+          color: Colors.white.withOpacity(0.08),
+          border: Border.all(
+            color: Colors.white.withOpacity(0.15),
+          ),
+        ),
+        child: child,
+      ),
+    ),
+  );
+}
